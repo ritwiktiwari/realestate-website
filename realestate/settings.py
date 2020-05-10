4e59ea131222b5fd4c55b27659fb73dc9903ffed
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-10 20:16:53 +0530

    realtor and listing model and registration

--- realestate/settings.py
+++ realestate/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestate',
+        'USER': 'postgres',
+        'PASSWORD': 'hello123',
+        'HOST': 'localhost',
+
     }
 }
 
