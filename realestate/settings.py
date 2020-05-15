2b0c3a32a531300ea41bd7f1f5386252c144ae8f
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-16 03:04:41 +0530

    removed email details from settings.py

--- realestate/settings.py
+++ realestate/settings.py
@@ -143,8 +143,8 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ritwiktiw@gmail.com'
-EMAIL_HOST_PASSWORD = '#24Fevicol'
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
