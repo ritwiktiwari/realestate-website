d8ef6be172a6c46ca39f747f5cc79475169ed3f3
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-16 02:59:01 +0530

    dshboard details

--- realestate/settings.py
+++ realestate/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -141,3 +142,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ritwiktiw@gmail.com'
+EMAIL_HOST_PASSWORD = '#24Fevicol'
+EMAIL_USE_TLS = True
